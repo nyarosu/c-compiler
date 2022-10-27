88b21e22f0bd74a2a08d7273176d6310962bc8cb
nyarosu <ronakc@fb.com>
2022-10-27 16:04:54 +0100

    auhgioaurghuaeogha

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -34,7 +34,14 @@ std::vector<Token> lex(std::string filePath) {
 
 // Checks if bufferToCheck is a valid token, and adds it to tokens if it is, then clears the buffer (if valid token).
 void checkBuffer(std::string &bufferToCheck, std::vector<Token> &tokens) {
-    std::set<std::string> validTokens {"{", "}", "(", ")", ";", "int", "return", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+  static std::set<std::string> validTokens;
+  std::array<std::string> toInsert {"{", "}", "(", ")", ";", "int", "return", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+  for (std::string element : toInsert) {
+    validTokens.insert(element);
+  }
+
+
 
     if (validTokens.find(bufferToCheck) == validTokens.end()) {
         tokens.push_back(bufferToCheck);
