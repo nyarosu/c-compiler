3b801743529e402cfb25da5ef08c495f319a1751
nyarosu <ronak.chatterjee65@gmail.com>
2022-10-27 07:37:57 +0100

    Add lexer.cpp

--- src/token.h
+++ src/token.h
@@ -2,18 +2,11 @@
 // Created by Ronak Chatterjee on 27/10/2022.
 //
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <set>
-
-
 #ifndef C_COMPILER_TOKEN_H
 #define C_COMPILER_TOKEN_H
 
 // Make set static, so that we don't initialize it on every call to checkBuffer
-static std::set<std::string> validTokens{{"{", "}", "(", ")", ";", "int", "return", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}};
+static std::set<std::string> validTokens;
 
 class Token {
 
