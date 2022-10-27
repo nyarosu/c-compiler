6e7c60ffcbce79a38372dd1d94c05c82d4f30e62
nyarosu <ronakc@fb.com>
2022-10-27 14:36:20 +0100

    a

--- src/token.h
+++ src/token.h
@@ -5,11 +5,10 @@
 #ifndef C_COMPILER_TOKEN_H
 #define C_COMPILER_TOKEN_H
 
-static std::set<std::string> validTokens;
-
 class Token {
 
 public:
+    static std::set<std::string> validTokens {"{", "}", "(", ")", ";", "int", "return", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
     Token(std::string data) {
         //todo
     }
