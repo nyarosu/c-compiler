6358959d290344df3a23bc1966d98e03c92e8174
nyarosu <ronak.chatterjee65@gmail.com>
2022-10-27 07:32:27 +0100

    Add lexer.cpp

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -36,7 +36,7 @@ std::vector<std::string> lex(std::string filePath) {
 // Checks if bufferToCheck is a valid token, and adds it to tokens if it is, then clears the buffer (if valid token).
 void checkBuffer(std::string &bufferToCheck, std::vector<std::string> &tokens) {
 
-    if (validTokens.contains(bufferToCheck)) {
+    if (validTokens.find(bufferToCheck) == validTokens.end()) {
         tokens.push_back(bufferToCheck);
     }
 
